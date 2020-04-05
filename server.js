1af8fd5eaf33c314e240eb3d356b08b6472f4a72
rodley82 <rodolfo.leyes@gmail.com>
2020-04-04 23:11:27 -0300

    - Print visitors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 let broadcaster;
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const http = require("http");
 const server = http.createServer(app);
+
+let watchers = 0;
 
 const io = require("socket.io")(server);
 app.use(express.static(__dirname + "/public"));
@@ -17,18 +19,25 @@ io.sockets.on("connection", socket => {
     socket.broadcast.emit("broadcaster");
   });
   socket.on("watcher", () => {
+    watchers+=1;
+    console.log("watcher watchers " + watchers);
     socket.to(broadcaster).emit("watcher", socket.id);
   });
   socket.on("offer", (id, message) => {
+    console.log("offer");
     socket.to(id).emit("offer", socket.id, message);
   });
   socket.on("answer", (id, message) => {
+    console.log("answer");
     socket.to(id).emit("answer", socket.id, message);
   });
   socket.on("candidate", (id, message) => {
+    console.log("candidate");
     socket.to(id).emit("candidate", socket.id, message);
   });
   socket.on("disconnect", () => {
+    watchers-=1;
+    console.log("disconnect watchers " + watchers);
     socket.to(broadcaster).emit("disconnectPeer", socket.id);
   });
 });
